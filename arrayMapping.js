55cdbc2f04379296bc7a2ca460e912b23952b203
samsarangi12 <soumyashree.sarangi@gmail.com>
2019-04-04 16:30:30 +0000

    array mapping assignment

---
+++ arrayMapping.js
@@ -0,0 +1,14 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map(function(num){
+  let squared = Math.pow(num.x, 2) + Math.pow(num.y, 2);
+  return Math.sqrt(squared);
+});
+
+//function squared(input1, input2)
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
